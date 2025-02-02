e270fc463746c54efcbe2da19de88d6bc229f63a
나경준 <testtesttestyo200@gmail.com>
2025-02-02 22:45:16 +0900

    clone: 학습완료

---
+++ practical-kotlin-multiplatform-on-web/src/jsMain/kotlin/todoapp/TodoClientApplication.kt
@@ -0,0 +1,33 @@
+package todoapp
+
+import kotlinx.browser.document
+import kotlinx.html.dom.create
+import kotlinx.html.h1
+import kotlinx.html.js.div
+import react.create
+import react.dom.client.createRoot
+import todoapp.ui.App
+import todoapp.ui.welcome.WelcomePage
+
+/**
+ * 클라이언트 애플리케이션 진입점(entry point)
+ *
+ * @author springrunner.kr@gmail.com
+ */
+fun main() {
+    val container = document.getElementById("root") ?: error("Couldn't find root container!")
+
+    createRoot(container=container).render(
+        App.create()
+    )
+//WelcomePage.create{
+//            name="Arawn Park"
+//        }
+//    container.appendChild(
+//        document.create.div{
+//            h1{
+//                +"Hello, Kotlin/JS!"
+//            }
+//        }
+//    )
+}
---
+++ practical-kotlin-multiplatform-on-web/src/jsMain/kotlin/todoapp/ui/welcome/WelcomePage.kt
@@ -0,0 +1,43 @@
+package todoapp.ui.welcome
+
+import csstype.*
+import emotion.react.css
+
+import react.FC
+import react.Props
+import react.dom.html.InputType
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.input
+import react.dom.html.ReactHTML.section
+import react.useState
+
+external interface WelcomePageProps : Props{
+    var name: String?
+}
+
+val WelcomePage = FC<WelcomePageProps> {
+    val(name,setName)= useState(it.name?:"SpringRunner")
+
+    section {
+        css {
+            position = Position.absolute
+            top = 30.pct
+            transform = translate(0.px, (-50).pct)
+        }
+        div {
+            css {
+                paddingBottom = 8.px
+                fontSize = 20.px
+                fontWeight = FontWeight.bold
+            }
+            +"Hello, $name"
+        }
+        input{
+            type = InputType.text;
+            value = name
+            onChange = {event->
+                setName(event.currentTarget.value)
+            }
+        }
+    }
+}
